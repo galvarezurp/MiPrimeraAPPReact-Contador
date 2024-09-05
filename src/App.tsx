978dea4c3b9277c7cc21c82bb80beeb12d76c8c7
Giancarlo Joel Alvarez Gallardo <giancarloag09@outlook.com>
2024-09-05 02:23:39 -0500

    Cambio de titulo de contador app

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     
     <main >
       <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/10/typescript-cover.jpg" width={400} alt="" />
-      <h1>Contador en React usando Typescript y TSX</h1>
+      <h1>Contador en React TS</h1>
       {/* Aqui */}
       {/*<BasicTypes/>*/}
       {/* <ObjectLiterals/> */}
