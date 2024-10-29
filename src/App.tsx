21363408bd0deaef14b5d8001eadf66b5d05e1c9
Giancarlo Joel Alvarez Gallardo <giancarloag09@gmail.com>
2024-10-29 04:13:52 -0500

    Subida cambio de titulo

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     
     <main >
       <img src="https://b2265571.smushcdn.com/2265571/wp-content/uploads/2022/07/ReactJS.png?lossy=2&strip=1&webp=1" width={400} alt="" />
-      <h1>Contador en React Framework con Typescript</h1>
+      <h1>Contador en React Framework con Typescript usando node</h1>
       {/* Aqui */}
       {/*<BasicTypes/>*/}
       {/* <ObjectLiterals/> */}
